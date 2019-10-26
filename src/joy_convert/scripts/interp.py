76e41de8745007dd9d759233a213771693fbce50
Kurt <kurt@localhost>
2019-10-26 16:28:18 -0400

    Adding subscribers to help calibrate min/max angles during testing

--- src/joy_convert/scripts/interp.py
+++ src/joy_convert/scripts/interp.py
@@ -7,21 +7,39 @@ from scipy import interpolate
 class image_transform(object):
 
     def __init__(self):
+        
         rospy.init_node('image_transform')
+       
+        # Params
         self.hz = rospy.get_param('~hz',1.0)
         self.rate = rospy.Rate(self.hz)
-
         self.minAngle = rospy.get_param('~minAngle',550)
         self.maxAngle = rospy.get_param('~maxAngle',2500)
         self.minPixel = rospy.get_param('~minPixel',0)
         self.maxPixel = rospy.get_param('~maxPixel',1280)
 
-        xs = [self.minPixel,self.maxPixel]
-        ys = [self.minAngle,self.maxAngle]
-        self.interpFunc = interpolate.interp1d(xs,ys,bounds_error=False,fill_value=(self.minAngle,self.maxAngle))
+        self.updateInterpFunction()
 
+        # Subs
         rospy.Subscriber('input',UInt16,self.setInput)
+        rospy.Subscriber('setMinAngle',UInt16,self.setMinAngle)
+        rospy.Subscriber('setMaxAngle',UInt16,self.setMaxAngle)
+
+        # Pubs
         self.servoPub = rospy.Publisher('servo',UInt16,queue_size=10)
+
+    def updateInterpFunction(self):
+       xs = [self.minPixel,self.maxPixel]
+       ys = [self.minAngle,self.maxAngle]
+       self.interpFunc = interpolate.interp1d(xs,ys,bounds_error=False,fill_value=(self.minAngle,self.maxAngle))
+
+    def setMinAngle(self,msg):
+        self.minAngle = msg.data
+        self.updateInterpFunction()
+
+    def setMaxAngle(self,msg):
+        self.maxAngle = msg.data
+        self.updateInterpFunction()
 
     def setInput(self,msg):
         cmd = self.interpFunc(msg.data)
--- src/joy_convert/scripts/state_machine.py
+++ src/joy_convert/scripts/state_machine.py
@@ -6,23 +6,36 @@ from std_msgs.msg import *
 class state_machine(object):
 
     def __init__(self):
+
         rospy.init_node('state_machine')
+
+        # Params
         self.hz = rospy.get_param('~hz',10.0)
         self.rate = rospy.Rate(self.hz)
         self.timeoutDuration = rospy.get_param('~timeoutDuration',5.0)
+
         self.yHomeVal = rospy.get_param('~y_home_val',1000)
-        self.yPointVal = rospy.get_param('~y_point_val',1500)
+        self.yPointVal = rospy.get_param('~y_point_val',1800)
 
         self.motionDetected = False
         self.lastMotionTs = None 
 
+        # Subs
         rospy.Subscriber('x_in',UInt16,self.setX)
+        rospy.Subscriber('set_y_home',UInt16,self.setYHome)
+        rospy.Subscriber('set_y_point',UInt16,self.setYPoint)
 
+        # Pubs
         self.yPub = rospy.Publisher('y_out',UInt16,queue_size=10)
-
         self.motionPub = rospy.Publisher('motionDetected',Bool,queue_size=10)
         self.eyePub = rospy.Publisher('eyeEnable',Bool,queue_size=10)
         self.soundPub = rospy.Publisher('soundEnable',Bool,queue_size=10)
+
+    def setYHome(self,msg):
+        self.yHomeVal = msg.data
+
+    def setYPoint(self,msg):
+        self.yPointVal = msg.data
 
     def setMotion(self,motion):
         if motion:
